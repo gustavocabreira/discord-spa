c4a2d47630af29b97c731822b2a7316de486c580
Gustavo Cabreira <gustavo.softdev@gmail.com>
2024-06-20 13:12:50 -0300

    style: login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="flex items-center justify-center w-[100vw] h-[100vh] bg-zinc-700">
+  <div
+    class="flex items-center justify-center w-[100vw] h-[100vh] bg-dark-grey"
+  >
     <form
-      class="w-full sm:w-1/2 md:w-1/3 mx-5 md:mx-0 bg-neutral-800 px-7 py-7 rounded-lg shadow gap-4 flex flex-col"
+      class="w-full sm:w-1/2 md:w-1/3 mx-5 md:mx-0 bg-neutral-800 px-7 py-7 rounded-lg shadow-lg gap-4 flex flex-col bg-grey"
       @submit.prevent="submit"
     >
       <div>
-        <p class="text-white text-center text-2xl">Boas-vindas de volta!</p>
-        <p class="text-center text-gray-300 mt-2">
+        <p class="text-white text-center font-bold text-2xl">
+          Boas-vindas de volta!
+        </p>
+        <p class="text-white text-center text-gray-300 mt-2">
           Estamos muito animados em te ver novamente!
         </p>
       </div>
@@ -15,7 +19,7 @@
           >E-MAIL <span class="text-red-500">*</span></label
         >
         <input
-          class="h-[40px] bg-zinc-900 rounded text-white px-2"
+          class="h-[40px] bg-dark-grey rounded text-snow px-2"
           v-model="email"
           type="email"
         />
@@ -25,14 +29,14 @@
           >SENHA <span class="text-red-500">*</span></label
         >
         <input
-          class="h-[40px] bg-zinc-900 rounded text-white px-2"
+          class="h-[40px] bg-dark-grey rounded text-snow px-2"
           v-model="password"
           type="password"
         />
-        <a href="#" class="text-blue-500 text-md">Esqueceu a sua senha?</a>
+        <a href="#" class="text-teal text-md">Esqueceu a sua senha?</a>
       </div>
       <button
-        class="btn w-full bg-indigo-800 hover:bg-indigo-400 text-white py-3 rounded-md transition ease-in-out"
+        class="btn w-full bg-teal hover:bg-indigo-400 text-white py-3 rounded-md transition ease-in-out"
         type="submit"
       >
         Entrar
@@ -60,6 +64,8 @@ const submit = async () => {
       password: password.value,
     });
   } catch (error) {
+    email.value = "";
+    password.value = "";
     alert(error.response._data.message ?? error.response._data.error);
   }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'dark-grey': '#222831',
+      'grey': '#31363F',
+      'teal': '#76ABAE',
+      'snow': '#DDD',
+    }
   },
   plugins: []
 }

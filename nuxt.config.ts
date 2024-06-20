662b9887e2525ffe42a438fc374fc2f510bbbc23
Gustavo Cabreira <gustavo.softdev@gmail.com>
2024-06-20 13:52:16 -0300

    wip: main screen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-auth-sanctum"],
+  modules: [
+    "nuxt-auth-sanctum",
+  ],
   sanctum: {
     baseUrl: 'http://localhost',
     redirectIfAuthenticated: true,
